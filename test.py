d81a400c6bd2a9b6578706383131b2332e2f7fa5
Dimondwill <damianr6564@nycstudents.net>
2025-09-10 10:19:52 -0400

    Update test.py
    dbgd

--- test.py
+++ test.py
@@ -5,6 +5,34 @@ t = Turtle()
 
 t.shape('turtle')
 
-t.forward(200)
+def rectangle(length, height):
+    t.forward(length)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+    t.forward(length)
+    t.left(90)
+    t.forward(height)
+    t.left(90)
+rectangle(100, 125)
 
+turtle.clearscreen()
+
+import turtle
+from turtle import *
+t = Turtle()
+
+
+t.shape('turtle')
+
+def triangle(four):
+    t.forward(four)
+    t.left(120)
+    t.forward(four)
+    t.left(120)    
+    t.forward(four)
+    t.left(120)    
+triangle(90)
+    
+    
 turtle.done()
