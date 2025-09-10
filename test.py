c4041d8c0b2b0d9364ef69542a6d04978ef6bcfa
Dimondwill <damianr6564@nycstudents.net>
2025-09-10 10:31:27 -0400

    many squares.py

--- test.py
+++ test.py
@@ -1,38 +1,8 @@
-import turtle
-from turtle import *
-t = Turtle()
-
-
-t.shape('turtle')
-
-def rectangle(length, height):
-    t.forward(length)
-    t.left(90)
-    t.forward(height)
-    t.left(90)
-    t.forward(length)
-    t.left(90)
-    t.forward(height)
-    t.left(90)
-rectangle(100, 125)
-
-turtle.clearscreen()
-
-import turtle
-from turtle import *
-t = Turtle()
-
-
-t.shape('turtle')
-
-def triangle(four):
-    t.forward(four)
-    t.left(120)
-    t.forward(four)
-    t.left(120)    
-    t.forward(four)
-    t.left(120)    
-triangle(90)
-    
-    
-turtle.done()
+def msqu(iRange):
+    length=10
+    angle=90
+    for i in range(iRange):
+        squ(length,angle)
+        length += 10
+        angle += 5
+msqu(5)
