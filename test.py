7804a47390834b83af3c8f064a9a7d030dee8f57
Dimondwill <damianr6564@nycstudents.net>
2025-09-11 07:32:28 -0700

    many squares

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+t.speed(1000)
+import math
+
 def msqu(iRange):
-    length=10
-    angle=90
+    length=2
+    angle=5
     for i in range(iRange):
-        squ(length,angle)
-        length += 10
-        angle += 5
-msqu(5)
+      t.forward(length)
+      t.left(90)
+      t.forward(length)
+      t.left(90)
+      t.forward(length)
+      t.left(90)
+      t.forward(length)
+      t.length(angle)
+
+ 
+        
+
+msqu(100)
+
+turtle.done()
