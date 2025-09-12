a35f419c9dcc682315016be3111b7c9c5532c016
Dimondwill <damianr6564@nycstudents.net>
2025-09-12 06:52:17 -0700

    Unit 1 complete

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import turtle
 from turtle import *
 t = Turtle()
 t.shape('turtle')
-t.speed(1000)
+t.speed(10000)
 import math
 
-def msqu(iRange):
-    length=2
+'''def msqu(iRange):
+    length=5
     angle=5
     for i in range(iRange):
       t.forward(length)
@@ -16,11 +16,33 @@ def msqu(iRange):
       t.forward(length)
       t.left(90)
       t.forward(length)
-      t.length(angle)
+      t.left(90)
+      t.right(angle)
+      length += 5
+msqu(60)'''
 
- 
-        
+'''def mstar(irange):
+    length=25
+    angle=144
+    for i in range(irange):
+        t.forward(length)
+        t.left(angle)
+        t.forward(length)
+        t.left(angle)
+        t.forward(length)
+        t.left(angle)
+        t.forward(length)
+        t.left(angle)
+        t.forward(length)
+        t.left(angle)
+        t.right(5)
+        length += 5
+mstar(60)'''
 
-msqu(100)
+
+
+
+
+
 
 turtle.done()
