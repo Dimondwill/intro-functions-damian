02ed5bebae5f90c63dedb007c921819556f8486f
Dimondwill <damianr6564@nycstudents.net>
2025-09-16 07:16:41 -0700

    almost done with 2

--- test.py
+++ test.py
@@ -1,27 +1,30 @@
-import turtle
-from turtle import *
-t = Turtle()
-t.shape('turtle')
-t.speed(10000)
-import math
+import random
 
+'''rannum = random.randint(1, 10)  # Generates an integer between 1 and 10 (inclusive)
+inputnum = int(input("Guess a number between 1 and 10: "))
+if inputnum == rannum:
+    print("You guessed it right!")'''
+
+import turtle
+t = turtle.Turtle()
+t.speed(100000000)
 '''def msqu(iRange):
     length=5
     angle=5
     for i in range(iRange):
       t.forward(length)
-      t.left(90)
+
+@@ -16,11 +16,33 @@ def msqu(iRange):
       t.forward(length)
       t.left(90)
       t.forward(length)
-      t.left(90)
-      t.forward(length)
+      t.length(angle)
       t.left(90)
       t.right(angle)
       length += 5
 msqu(60)'''
 
-'''def mstar(irange):
+def mstar(irange):
     length=25
     angle=144
     for i in range(irange):
@@ -37,12 +40,6 @@ msqu(60)'''
         t.left(angle)
         t.right(5)
         length += 5
-mstar(60)'''
-
-
-
-
-
-
+mstar(60)
 
 turtle.done()
